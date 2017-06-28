6fa13923a0ba9fd881730507e19f2bca69894d88
java2019 <java_2019@mail.ru>
2017-06-28 16:06:54 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/location/reversegeocodingmap/GeocoderActivity.java
@@ -0,0 +1,13 @@
+package com.samples.location.reversegeocodingmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeocoderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geocoder);
+    }
+}
